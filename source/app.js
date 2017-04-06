ed6139c8162ac3058f862dfaa3398a0fbceeecd3
Rana Das <rana.pratap.das@gmail.com>
2017-04-06 08:37:45 +0100

    working hello world

--- source/app.js
+++ source/app.js
@@ -2,5 +2,33 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
-const App = () => <h1>Hello World!</h1>;
+import * as Home from './component/home';
+import * as Address from './component/address';
+
+const AppNowDeprecated = () => <h1>Hello World!</h1>;
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Hello Rana Das!</h1>
+        );
+    }
+}
+
+
 export default App;
+/*
+
+
+ class App extends Component {
+ render() {
+ return (
+ <Router history={hashHistory}>
+ <Route path='/' component={Home} />
+ <Route path='/address' component={Address} />
+ </Router>
+ )
+ }
+ }
+
+ */
